3cda03d129082f836b5b4b7c92a275b9fe0005e5
Oleh <my.mail.olech@gmail.com>
2021-08-03 17:48:19 +0300

    10

---
+++ 8.objects-keys-and-values.js
@@ -0,0 +1,6 @@
+// Create a function that takes an object and returns the keys and values as separate arrays. Return the keys sorted alphabetically, and their corresponding values in the same order.
+
+const keysAndValues = (obj = {}) => {
+  const objSortedKeys = Object.keys(obj).sort();
+  return [objSortedKeys, Object.values(obj)];
+};
