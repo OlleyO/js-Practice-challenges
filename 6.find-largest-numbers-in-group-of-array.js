2382ec71f0490ea657573383e23487400af5d37d
Oleh <my.mail.olech@gmail.com>
2021-08-03 17:07:25 +0300

    half done

---
+++ 6.find-largest-numbers-in-group-of-array.js
@@ -0,0 +1,10 @@
+// Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+
+const findLargestNums = (arr = []) => {
+  const maxArr = [];
+  for (const subArr of arr) {
+    maxArr.push(Math.max(...subArr));
+  }
+
+  return maxArr;
+};
