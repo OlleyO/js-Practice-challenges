a52ce6fff17323fb41c6374761e83ac2d5362133
Ruslan Hontar <hontar.ruslan@gmail.com>
2021-08-30 21:16:40 +0300

    clean code

--- 1.how-much-is-true.js
+++
@@ -1,6 +0,0 @@
-// Create a function which returns the number of true values there are in an array.
-const countTrue = (array = []) => {
-  const convertedToBoolean = array.map((elem) => !!elem);
-  console.log("Boolean array: ", convertedToBoolean);
-  return convertedToBoolean.filter((elem) => elem === true).length;
-};
--- 4.sort-array-by-string-length.js
+++
@@ -1,8 +0,0 @@
-// Create a function that takes an array of strings and return an array, sorted from shortest to longest.
-const sortFunc = (elem1, elem2) => {
-  return elem1.length - elem2.length;
-};
-
-const sortByLength = (arrayOfStrings = []) => {
-  return arrayOfStrings.sort(sortFunc);
-};
